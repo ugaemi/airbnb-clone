beefcea8e2a606e1bb89f9fdc27c92c34cedafb9
ugaemi <u.gaemi@gmail.com>
2020-06-21 21:51:40 +0900

    Lecture 6

--- rooms/admin.py
+++ rooms/admin.py
@@ -16,7 +16,60 @@ class RoomAdmin(admin.ModelAdmin):
 
     """Room Admin Definition"""
 
-    pass
+    fieldsets = (
+        (
+            "Basic Info",
+            {"fields": ("name", "description", "country", "address", "price")},
+        ),
+        (
+            "Times",
+            {"fields": ("check_in", "check_out", "instant_book")},
+        ),
+        (
+            "More About the Space",
+            {
+                "classes": ("collapse",),
+                "fields": ("amenities", "facilities", "house_rules")
+            },
+        ),
+        (
+            "Spaces",
+            {"fields": ("guests", "beds", "bedrooms", "baths")},
+        ),
+        (
+            "Last Details",
+            {"fields": ("host",)},
+        ),
+    )
+
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+    )
+
+    list_filter = (
+        "instant_book",
+        "host__superhost",
+        "room_type",
+        "amenities",
+        "facilities",
+        "house_rules",
+        "city",
+        "country",
+    )
+
+    search_fields = ("=city", "^host__username")
+
+    filter_horizontal = ("amenities", "facilities", "house_rules")
 
 
 @admin.register(models.Photo)
