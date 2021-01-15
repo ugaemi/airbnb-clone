9a6678cb68c798c6724b009d6203d1501ace0987
ugaemi <u.gaemi@gmail.com>
2021-01-15 21:14:03 +0900

    Lecture 15.0 - 15.1

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,7 @@ from users import views
 app_name = "users"
 
 urlpatterns = [
-    path("login", views.LoginView.as_view(), name="login")
+    path("login", views.LoginView.as_view(), name="login"),
+    path("logout", views.log_out, name="logout"),
+    path("signup", views.SignUpView.as_view(), name="signup"),
 ]
